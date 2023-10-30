89fae5811cdfda7df67e3adc826186e1886eeea0
Timothy Nicholas <tsnicholas@bsu.edu>
2023-10-30 16:24:01 -0400

    Create DatabaseParser
    Co-authored-by: Ellie Bertke <elliebertke@gmail.com>
    Co-authored-by: Julio Orozco <jeorozco@bsu.edu>
    Co-authored-by: Megan Stinefield <megan.stinefield@bsu.edu>
    Co-authored-by: Victoria Moon <vanastasiamoon@gmail.com>

--- Parser/DatabaseParser.js
+++ Parser/DatabaseParser.js
@@ -1,16 +1,29 @@
 import { Client } from 'pg';
+const config = require("./config.json");
 
 export class DatabaseParser {
     constructor() {
-        const client  = new Client();
-        this.connectToClient(client);
+        this.client  = new Client();
+        this.connectToClient(this.client);
     }
 
     async connectToClient(client) {
-        client.connect();
+        client.connect({
+            host: "localhost",
+            user: config.USERNAME,
+            password: config.PASSWORD,
+            config: config.DATABASE,
+            port: config.PORT,
+        });
+    }
+    
+    async retrieveLogin(username, password) {
+        return await this.client.query(`SELECT * FROM ACCOUNT WHERE username = ${username}, account_password = ${password}`);  
+    }
+    
+    async storeLogin(username, email, password){
+        await this.client.query(`INSERT INTO ACCOUNT(username, account_password, email) VALUES (${username}, ${password}, ${email})`);
     }
 
-    retrieveLogin() {
-        
-    }
+    
 }
