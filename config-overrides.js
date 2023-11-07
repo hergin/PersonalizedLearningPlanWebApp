83a268b5fda29546578c04a784df5fad19905b0a
Timothy Nicholas <tsnicholas@bsu.edu>
2023-11-07 11:13:33 -0500

    Update techstack

---
+++ config-overrides.js
@@ -0,0 +1,23 @@
+const webpack = require('webpack');
+
+module.exports = function override(config) {
+    const fallback = config.resolve.fallback || {};
+    Object.assign(fallback, {
+        "stream": require.resolve("stream-browserify"),
+        "path": require.resolve("path-browserify"),
+        "crypto": require.resolve("crypto-browserify"),
+        "assert": require.resolve("assert"),
+        "os": require.resolve("os-browserify"),
+        "http": require.resolve("stream-http"),
+        "https": require.resolve("https-browserify"),
+        "url": require.resolve("url")
+    });
+    config.resolve.fallback = fallback;
+    config.plugins = (config.plugins || []).concat([
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+            Buffer: ['buffer', 'Buffer']
+        })
+    ])
+    return config; 
+}
