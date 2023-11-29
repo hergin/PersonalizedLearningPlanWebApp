7168691a5064856d2d1e8285272651a19b19ca07
Timothy Nicholas <tsnicholas@bsu.edu>
2023-11-29 16:21:23 -0500

    Update unit tests

--- server/tests/controller/loginProcessor.test.js
+++ server/tests/controller/loginProcessor.test.js
@@ -10,8 +10,6 @@ jest.mock("../../parser/DatabaseParser", () => {
         storeLogin: jest.fn(),
         storeToken: jest.fn(),
         parseToken: jest.fn(),
-        storeProfile: jest.fn(),
-        parseProfile: jest.fn(),
     };
     return jest.fn(() => testParser);
 });
@@ -31,11 +29,6 @@ describe('Login Functions', () => {
         password: "password",
         email: "George123@Gmail.com",
         refreshToken: "UTDefpAEyREXmgCkK04pL1SXK6jrB2tEc2ZyMbrFs61THq2y3bpRZOCj5RiPoZGa",
-        firstName: "George",
-        lastName: "Johnson",
-        profilePicture: "",
-        bio: "I'm george!",
-        jobTitle: "Unemployed"
     };
     
     let loginAPI;
@@ -115,63 +108,5 @@ describe('Login Functions', () => {
         parser.parseToken.mockResolvedValueOnce([{'refreshtoken': testData.refreshToken}]);
         expect(await loginAPI.verifyToken(testData.email, testData.refreshToken)).toEqual(STATUS_CODES.OK);
     });
-
-    it('create profile (pass case)', async () => {
-        parser.storeProfile.mockResolvedValueOnce();
-        expect(await profileAPI.createProfile(testData.username, testData.email, testData.password)).toEqual(STATUS_CODES.OK);
-    });
-
-    it('create profile (duplicate case)', async () => {
-        parser.storeProfile.mockRejectedValue({code: '23505'});
-        expect(await profileAPI.createProfile(testData.username, testData.email, testData.password)).toEqual(STATUS_CODES.CONFLICT);
-    });
-
-    it('create profile (bad data case)', async () => {
-        parser.storeProfile.mockRejectedValue({code: '23514'});
-        expect(await profileAPI.createProfile(testData.username, testData.email, testData.password)).toEqual(STATUS_CODES.BAD_REQUEST);
-    });
-
-    it('create profile (connection lost case)', async () => {
-        parser.storeProfile.mockRejectedValue({code: '08000'});
-        expect(await profileAPI.createProfile(testData.username, testData.email, testData.password)).toEqual(STATUS_CODES.CONNECTION_ERROR);
-    });
-
-    it('create profile (fatal error case)', async () => {
-        parser.storeProfile.mockRejectedValue({code: 'adsfa'});
-        expect(await profileAPI.createProfile(testData.username, testData.email, testData.password)).toEqual(STATUS_CODES.INTERNAL_SERVER_ERROR);
-    });
-
-    it('get profile (pass case)', async () => {
-        parser.parseProfile.mockResolvedValueOnce([
-            {
-                firstname: testData.firstName, lastname: testData.lastName, profilepicture: testData.profilePicture, 
-                jobtitle: testData.jobTitle, bio: testData.bio, email: testData.email
-            }
-        ]);
-        expect(await loginAPI.getProfile(testData.email)).toEqual([
-            {
-                firstname: testData.firstName, lastname: testData.lastName, profilepicture: testData.profilePicture,
-                jobtitle: testData.jobTitle, bio: testData.bio, email: testData.email
-            }
-        ]);
-        expect(await profileAPI.getProfile(testData.email)).toEqual({
-            firstname: testData.firstName, lastname: testData.lastName, profilepicture: testData.profilePicture,
-            jobtitle: testData.jobTitle, bio: testData.bio, email: testData.email
-        });
-    });
-
-    it('get profile (profile missing case)', async () => {
-        parser.parseProfile.mockResolvedValueOnce([]);
-        expect(await profileAPI.getProfile(testData.email)).toEqual(STATUS_CODES.UNAUTHORIZED);
-    });
-
-    it('get profile (connection lost case)', async () => {
-        parser.parseProfile.mockRejectedValue({code: '08000'});
-        expect(await profileAPI.getProfile(testData.email)).toEqual(STATUS_CODES.CONNECTION_ERROR);
-    });
-
-    it('get profile (fatal error case)', async () => {
-        parser.parseProfile.mockRejectedValue({code: 'adfads'});
-        expect(await profileAPI.getProfile(testData.email)).toEqual(STATUS_CODES.INTERNAL_SERVER_ERROR);
-    });
 });
+    
