b76da34fd4690fa7b9f2bee8a3c3fc6ccb5efcc8
jeorozc0 <jeorozco@bsu.edu>
2023-12-02 16:09:41 -0500

    Fix ModuleComponent.jsx values

--- client/src/components/ModuleComponent/ModuleComponent.jsx
+++ client/src/components/ModuleComponent/ModuleComponent.jsx
@@ -1,39 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ModuleCreator from "../ModuleCreator";
 import { ApiClient } from "../../hooks/ApiClient";
 import { useUser } from "../../hooks/useUser";
 import "./ModuleComponent.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const ModuleComponent = () => {
   const [modules, setModules] = useState([]);
   const { user } = useUser();
   const { get } = ApiClient();
-  
+
   useEffect(() => {
     async function getModules() {
       try {
-        console.log(`User: ${user.email} ${user.accessToken} ${user.refreshToken}`);
+        console.log(
+          `User: ${user.email} ${user.accessToken} ${user.refreshToken}`,
+        );
         const result = await get(`/module/get/${user.email}`);
         console.log(`Resulting data: ${result}`);
         var newModules = [];
-        for(var module of result) {
-          console.log(`Adding ${module.module_name}`)
+        for (var module of result) {
+          console.log(`Adding ${module.module_name}`);
           newModules.push(module);
         }
         setModules(newModules);
-      } catch(error) {
+      } catch (error) {
         console.error(error);
-        alert((error.message) ? error.message : error);
+        alert(error.message ? error.message : error);
       }
     }
-    
+
     getModules();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
+
   function addModule(module) {
-    if(modules.includes(module)) {
+    if (modules.includes(module)) {
       return;
     }
     var newModules = [].concat(modules);
@@ -49,25 +51,32 @@ const ModuleComponent = () => {
           <Module
             key={`ID-${module.module_id}`}
             moduleId={module.module_id}
-            goalName={module.module_name}
-            goalDescription={module.description}
-            completion={module.completion_percent}
+            moduleName={module.module_name}
+            moduleDescription={module.description}
+            moduleCompletion={module.completion_percent}
           />
         ))}
-        <ModuleCreator addModule={addModule}/>
+        <ModuleCreator addModule={addModule} />
       </div>
     </button>
   );
 };
 
-const Module = ({moduleId, moduleName, moduleDescription, moduleCompletion }) => {
+const Module = ({
+  moduleId,
+  moduleName,
+  moduleDescription,
+  moduleCompletion,
+}) => {
   return (
     <Link to={`/goals/${moduleId}`} className="module-div">
       <div className="module-header">
         <h1>{moduleName}</h1>
       </div>
       <div className="module-body">
-        <p>Progress: {moduleCompletion === 100 ? "Completed" : "In progress"}</p>
+        <p>
+          Progress: {moduleCompletion === 100 ? "Completed" : "In progress"}
+        </p>
         <p>Description:</p>
         <p>{moduleDescription}</p>
       </div>
