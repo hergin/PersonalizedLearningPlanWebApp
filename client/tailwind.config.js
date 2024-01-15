eb49f548b7abc12d1484989cae36fa00b7420a9c
jeorozc0 <jeorozco@bsu.edu>
2024-01-14 22:15:05 -0500

    fix tailwind themes and register position

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "element-base": "var(--mainColor)",
+        "text-color": "var(--mainColorHighlight)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
