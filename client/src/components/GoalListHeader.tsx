2e6c6b0e4c5008fc20fddcfb6be983df91720a38
jeorozc0 <jeorozco@bsu.edu>
2024-01-30 09:43:47 -0500

    Refactor GoalItem component and add GoalListHeader

---
+++ client/src/components/GoalListHeader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+export function GoalListHeader() {
+  return (
+    <div className="flex flex-row transition-transform rounded  w-full ">
+      <div className="flex flex-col w-2/5 h-full justify-center p-3 ">
+        <p className="text-[#888888] text-lg font-headlineFont">Goal</p>
+      </div>
+      <div className="flex flex-col transition-transform w-[15%] h-full justify-center p-3 items-center">
+        <p className="text-[#888888] font-headlineFont">Start Date</p>
+      </div>
+      <div className="flex flex-col transition-transform w-[15%] h-full justify-center p-3 items-center">
+        <p className="text-[#888888] headlineFont">End Date</p>
+      </div>
+      <div className="flex flex-col transition-transform w-[15%] h-full justify-center p-3 items-center">
+        <p className="text-[#888888] headlineFont">Progress</p>
+      </div>
+      <div className="flex flex-col transition-transform w-[15%] h-full justify-center p-3 items-center">
+        <p className="text-black"></p>
+      </div>
+    </div>
+  );
+}
