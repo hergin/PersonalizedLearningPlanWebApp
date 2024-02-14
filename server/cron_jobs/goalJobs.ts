9c84112918d2c961fea2db75ce1be5ad1bd66320
tsnicholas <tsnicholas@bsu.edu>
2024-02-14 15:46:36 -0500

    Create cron job to email users about due dates

--- server/cron_jobs/goalJobs.ts
+++ server/cron_jobs/goalJobs.ts
@@ -1,9 +1,23 @@
 import { schedule } from "node-cron";
+import EmailService from "@server/service/emailService";
 import GoalParser from "@server/parser/goalParser";
 
 const EVERYDAY_AT_MIDNIGHT : string = "0 0 * * *";
 const parser = new GoalParser();
+const emailService = new EmailService();
 
-const updateDatabase = schedule(EVERYDAY_AT_MIDNIGHT, () => {
-    
+const notifyOfCloseDueDates = schedule(EVERYDAY_AT_MIDNIGHT, async () => {
+    const results = await parser.parseAccountsWithUpcomingDueDates();
+    for(const user of results) {
+        const message = generateMessage(user.goal, user.username, new Date(user.due_date));
+        emailService.sendEmail(user.email, "Due Date Coming Soon!", message);
+    }
 });
+
+function generateMessage(goalName: string, username: string, dueDate: Date):string {
+    return `
+        <p>Hello ${username},</p>
+        <p>Your due date for ${goalName} is coming up at ${dueDate.toDateString()} on ${dueDate.getTime()}.</p>
+        <p>Make sure to complete your goals on time!</p>
+    `
+}
