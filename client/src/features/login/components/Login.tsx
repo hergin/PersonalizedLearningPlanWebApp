a6205c5d43635de208a941ac4ab225225ffa5f02
tsnicholas <tsnicholas@bsu.edu>
2024-02-16 14:38:16 -0500

    Refactor problems in client side

--- client/src/features/login/components/Login.tsx
+++ client/src/features/login/components/Login.tsx
@@ -86,7 +86,7 @@ const LoginScreen = () => {
         </div>
       </div>
       <div className="flex mt-2 flex-col justify-center items-center h-24 w-[300px] border border-solid border-[#DBDBDB]">
-        <p>Don't have an account?</p>
+        <p>Don&apos;t have an account?</p>
         <Link
           to="/register"
           className="no-underline text-blue-700 text-base mb-2"
