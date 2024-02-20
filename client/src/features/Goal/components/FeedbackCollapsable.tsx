580ff0347b78d9ac8c52b2c1c3b22126e458eea5
Victoria Moon <vanastasiamoon@gmail.com>
2024-02-20 12:46:54 -0500

    Create handle of submit

--- client/src/features/Goal/components/FeedbackCollapsable.tsx
+++ client/src/features/Goal/components/FeedbackCollapsable.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, {useState} from "react";
+import { ApiClient } from "../../../hooks/ApiClient";
 
 interface FeedbackCollapsableProps{
     getCollapsableProps: any;
@@ -6,11 +7,22 @@ interface FeedbackCollapsableProps{
 }
 
 export default function FeedbackCollapsable({getCollapsableProps, feedback}: FeedbackCollapsableProps){
+    const [newFeedback, setFeedback] = useState <string>(feedback ? feedback : "");
+    const {put} = ApiClient();
+
+
+    function HandleSubmit(){
+
+    }
+    
     return (
         <div {...getCollapsableProps()}>
-            <textarea className="text-black">
+            <textarea className="text-black" value={newFeedback} onChange={(input) => setFeedback(input.target.value)}>
 
             </textarea>
+            <button onClick={HandleSubmit} className="text-black">
+                Submit
+            </button>
         </div>
     );
 }
