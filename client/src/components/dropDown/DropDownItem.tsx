d6e567e2591f02ba847550a055087895daa4ebaf
tsnicholas <tsnicholas@bsu.edu>
2024-02-21 20:10:12 -0500

    Refactor the design of drop down menu

--- client/src/components/dropDown/DropDownItem.tsx
+++ client/src/components/dropDown/DropDownItem.tsx
@@ -8,7 +8,7 @@ interface DropDownItemProps extends PropsWithChildren {
 
 export default function DropDownItem(props: DropDownItemProps) {
     return(
-        <div onClick={props.onClick} className={"h-[50px] p-[0.5rem] flex items-center hover:bg-[#820000] cursor-pointer duration-500"}>
+        <div onClick={props.onClick} className={"h-[50px] p-[1rem] flex items-center gap-2 hover:bg-[#820000] cursor-pointer duration-500"}>
             {props.leftIcon && <span className="icon-button">{props.leftIcon}</span>}
             {props.children}
             {props.rightIcon && <span className="icon-button">{props.rightIcon}</span>}
