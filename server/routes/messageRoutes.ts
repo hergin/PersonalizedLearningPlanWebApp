b35552fa3bc49183496d343c932839b49cae8ee4
tsnicholas <tsnicholas@bsu.edu>
2024-03-25 19:12:15 -0400

    Add message api to express routes

---
+++ server/routes/messageRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as MessageProcessor from "../controller/processors/messageProcessor";
+import { authenticateToken } from "../utils/token";
+
+const messageRoutes = Router();
+messageRoutes.get("/:id", authenticateToken, MessageProcessor.getAllSentMessages);
+messageRoutes.get("/:id/:receivedId", authenticateToken, MessageProcessor.getMessagesBetween);
+messageRoutes.post("/send", authenticateToken, MessageProcessor.postMessage);
+messageRoutes.delete("/delete", authenticateToken, MessageProcessor.deleteMessage);
+
+export default messageRoutes;
