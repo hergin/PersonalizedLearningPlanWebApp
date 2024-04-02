24a08834d6ad3478463a3255ce1761a78ecab06d
tsnicholas <tsnicholas@bsu.edu>
2024-04-02 10:54:46 -0400

    Update database and tsconfig files

--- server/routes/loginRoutes.ts
+++ server/routes/loginRoutes.ts
@@ -7,7 +7,7 @@ loginRoutes.post('/login', loginProcessor.verifyLogin);
 loginRoutes.post('/token', loginProcessor.verifyToken);
 loginRoutes.post('/register', loginProcessor.registerAccount);
 loginRoutes.post('/logout', loginProcessor.logoutUser);
-loginRoutes.delete('/delete/:id', loginProcessor.deleteAccount);
+loginRoutes.delete('/delete/:id', authenticateToken, loginProcessor.deleteAccount);
 loginRoutes.get('/understudy/:id', authenticateToken, loginProcessor.getUnderstudies);
 
 export default loginRoutes;
