45635fa63dfbbbf189da9eaef55ceba10df156f0
Megan237 <megan.stinefield@bsu.edu>
2024-04-03 00:22:12 -0400

    Added email notifications for Feedback service

--- server/controller/api/loginApi.ts
+++ server/controller/api/loginApi.ts
@@ -5,26 +5,26 @@ import { ErrorCodeInterpreter } from "./errorCodeInterpreter";
 import { DatabaseError } from "pg";
 
 export default class LoginAPI {
-    parser : LoginParser;
-    errorCodeInterpreter : ErrorCodeInterpreter;
+    parser: LoginParser;
+    errorCodeInterpreter: ErrorCodeInterpreter;
 
     constructor() {
-      this.parser = new LoginParser();
-      this.errorCodeInterpreter = new ErrorCodeInterpreter();
+        this.parser = new LoginParser();
+        this.errorCodeInterpreter = new ErrorCodeInterpreter();
     }
 
-    async verifyLogin(email : string, password : string): Promise<number | StatusCode> {
+    async verifyLogin(email: string, password: string): Promise<number | StatusCode> {
         try {
             const login = await this.parser.retrieveLogin(email);
             console.log(JSON.stringify(login));
-            if(login.length === 0) return StatusCode.GONE;
+            if (login.length === 0) return StatusCode.GONE;
             return await bcrypt.compare(password, login[0].account_password) ? login[0].id : StatusCode.UNAUTHORIZED;
         } catch (error: unknown) {
             return this.errorCodeInterpreter.getStatusCode(error as DatabaseError);
         }
     }
-    
-    async createAccount(email : string, password : string) {
+
+    async createAccount(email: string, password: string) {
         try {
             console.log(password);
             const hash = await this.#hashPassword(password);
@@ -36,12 +36,22 @@ export default class LoginAPI {
         }
     }
 
-    async #hashPassword(password : string) {
+    async getAccountById(accountId: number): Promise<any[] | StatusCode> {
+        try {
+            const email = await this.parser.getEmailById(accountId);
+            console.log(`Parsed account: \n${JSON.stringify(email)}`);
+            return email;
+        } catch (error: unknown) {
+            return this.errorCodeInterpreter.getStatusCode(error as DatabaseError);
+        }
+    }
+
+    async #hashPassword(password: string) {
         const salt = await bcrypt.genSalt(10);
         return await bcrypt.hash(password, salt);
     }
 
-    async setToken(accountId: number, refreshToken : string) {
+    async setToken(accountId: number, refreshToken: string) {
         try {
             await this.parser.storeToken(accountId, refreshToken);
             return StatusCode.OK;
@@ -50,17 +60,17 @@ export default class LoginAPI {
         }
     }
 
-    async verifyToken(accountId: number, refreshToken : string) {
+    async verifyToken(accountId: number, refreshToken: string) {
         try {
             const result = await this.parser.parseToken(accountId);
-            if(result.length === 0) return StatusCode.GONE;
+            if (result.length === 0) return StatusCode.GONE;
             return (result[0].refresh_token === refreshToken) ? StatusCode.OK : StatusCode.UNAUTHORIZED;
         } catch (error: unknown) {
             return this.errorCodeInterpreter.getStatusCode(error as DatabaseError);
         }
     }
 
-    async logout(accountId : number) {
+    async logout(accountId: number) {
         try {
             await this.parser.deleteToken(accountId);
             return StatusCode.OK;
@@ -69,7 +79,7 @@ export default class LoginAPI {
         }
     }
 
-    async delete(accountId : number) {
+    async delete(accountId: number) {
         try {
             await this.parser.deleteAccount(accountId);
             return StatusCode.OK;
@@ -81,7 +91,7 @@ export default class LoginAPI {
     async getUnderstudies(accountId: number) {
         try {
             return await this.parser.parseUnderstudies(accountId);
-        } catch (error : unknown) {
+        } catch (error: unknown) {
             return this.errorCodeInterpreter.getStatusCode(error as DatabaseError);
         }
     }
