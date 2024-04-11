43a3f4fb94528cf9b2b2955a58faf974f7d6168f
jeorozc0 <jeorozco@bsu.edu>
2024-04-10 20:48:47 -0400

    Refactor AdminTable component in AdminPanel feature to add CRUD functionality

--- client/src/features/admin-panel/components/AdminTable.tsx
+++ client/src/features/admin-panel/components/AdminTable.tsx
@@ -1,68 +1,205 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
-import { useAccountData } from "../../../hooks/useAdminFeatures";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/DeleteOutlined";
+import SaveIcon from "@mui/icons-material/Save";
+import CancelIcon from "@mui/icons-material/Close";
+import {
+  GridRowModesModel,
+  GridRowModes,
+  DataGrid,
+  GridColDef,
+  GridActionsCellItem,
+  GridEventListener,
+  GridRowId,
+  GridRowModel,
+  GridRowEditStopReasons,
+} from "@mui/x-data-grid";
+import {
+  useAccountData,
+  useRoleUpdater,
+} from "../../../hooks/useAdminFeatures";
+import { Role } from "../../../types";
 
-export default function DataGridDemo() {
-  const { data: accounts, isLoading, error } = useAccountData();
-  console.log(accounts)
-  const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 14 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 31 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 31 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 11 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-  ];
+export default function FullFeaturedCrudGrid() {
+  const { data: profiles, isLoading, error } = useAccountData();
+  const { mutateAsync: updateRole } = useRoleUpdater();
+  const [rows, setRows] = React.useState<GridRowModel[]>([]);
+  const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>(
+    {}
+  );
 
-  const columns: GridColDef<(typeof rows)[number]>[] = [
-    { field: "id", headerName: "ID", flex: 1 },
+  React.useEffect(() => {
+    if (profiles) {
+      setRows(profiles);
+    }
+  }, [profiles]);
+
+  const handleRowEditStop: GridEventListener<"rowEditStop"> = (
+    params,
+    event
+  ) => {
+    if (params.reason === GridRowEditStopReasons.rowFocusOut) {
+      event.defaultMuiPrevented = true;
+    }
+  };
+
+  const handleEditClick = (id: GridRowId) => () => {
+    setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
+  };
+
+  const handleSaveClick =
+    (id: GridRowId, account_id: number, role: Role) => async () => {
+      try {
+        await updateRole({ id: account_id, role: role });
+        setRowModesModel({
+          ...rowModesModel,
+          [id]: { mode: GridRowModes.View },
+        });
+      } catch (error) {
+        console.error("Error updating role:", error);
+      }
+    };
+
+  const handleDeleteClick = (id: GridRowId) => () => {
+    setRows(rows.filter((row) => row.account_id !== id));
+  };
+
+  const handleCancelClick = (id: GridRowId) => () => {
+    setRowModesModel({
+      ...rowModesModel,
+      [id]: { mode: GridRowModes.View, ignoreModifications: true },
+    });
+
+    const editedRow = rows.find((row) => row.account_id === id);
+    if (editedRow?.isNew) {
+      setRows(rows.filter((row) => row.account_id !== id));
+    }
+  };
+
+  const processRowUpdate = (newRow: GridRowModel) => {
+    const updatedRow = { ...newRow };
+    setRows(
+      rows.map((row) =>
+        row.account_id === newRow.account_id ? updatedRow : row
+      )
+    );
+    return updatedRow;
+  };
+
+  const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
+    setRowModesModel(newRowModesModel);
+  };
+
+  // Columns
+
+  const columns: GridColDef[] = [
+    { field: "username", headerName: "UserName", width: 180 },
     {
-      field: "firstName",
-      headerName: "First name",
-      flex: 1,
-      editable: true,
+      field: "email",
+      headerName: "Email",
+      width: 80,
+      align: "left",
+      headerAlign: "left",
     },
     {
-      field: "lastName",
-      headerName: "Last name",
-      flex: 1,
-      editable: true,
+      field: "account_id",
+      headerName: "Account ID",
+      width: 180,
     },
     {
-      field: "age",
-      headerName: "Age",
-      type: "number",
-      flex: 1,
+      field: "role",
+      headerName: "Department",
+      width: 220,
       editable: true,
+      type: "singleSelect",
+      valueOptions: ["basic", "admin"],
     },
     {
-      field: "fullName",
-      headerName: "Full name",
-      description: "This column has a value getter and is not sortable.",
-      sortable: false,
-      flex: 1,
+      field: "actions",
+      type: "actions",
+      headerName: "Actions",
+      width: 100,
+      cellClassName: "actions",
+      getActions: ({ id }) => {
+        const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
+        const currentRow = rows.find((row) => row.account_id === id);
+
+        if (isInEditMode) {
+          return [
+            <GridActionsCellItem
+              icon={<SaveIcon />}
+              label="Save"
+              sx={{
+                color: "primary.main",
+              }}
+              onClick={handleSaveClick(
+                id,
+                currentRow?.account_id,
+                currentRow?.role
+              )}
+              key={id}
+            />,
+            <GridActionsCellItem
+              icon={<CancelIcon />}
+              label="Cancel"
+              className="textPrimary"
+              onClick={handleCancelClick(id)}
+              color="inherit"
+              key={id}
+            />,
+          ];
+        }
+
+        return [
+          <GridActionsCellItem
+            icon={<EditIcon />}
+            label="Edit"
+            className="textPrimary"
+            onClick={handleEditClick(id)}
+            color="inherit"
+            key={id}
+          />,
+          <GridActionsCellItem
+            icon={<DeleteIcon />}
+            label="Delete"
+            onClick={handleDeleteClick(id)}
+            color="inherit"
+            key={id}
+          />,
+        ];
+      },
     },
   ];
 
   return (
-    <Box sx={{ height: 350, width: "100%" }}>
+    <Box
+      sx={{
+        height: 500,
+        width: "100%",
+        "& .actions": {
+          color: "text.secondary",
+        },
+        "& .textPrimary": {
+          color: "text.primary",
+        },
+      }}
+    >
       <DataGrid
         rows={rows}
         columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
-            },
-          },
+        getRowId={(row) => row.account_id}
+        loading={isLoading}
+        editMode="row"
+        rowModesModel={rowModesModel}
+        onRowModesModelChange={handleRowModesModelChange}
+        onRowEditStop={handleRowEditStop}
+        processRowUpdate={processRowUpdate}
+        slotProps={{
+          toolbar: { setRows, setRowModesModel },
         }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
       />
     </Box>
   );
