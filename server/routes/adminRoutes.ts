a23424d2b10311ca2de07205cdf7c86426fef563
tsnicholas <tsnicholas@bsu.edu>
2024-04-12 15:05:09 -0400

    Refactor for setting account role

--- server/routes/adminRoutes.ts
+++ server/routes/adminRoutes.ts
@@ -5,6 +5,6 @@ import * as AdminProcessor from "../controller/processors/adminProcessor";
 const adminRoutes = Router();
 adminRoutes.get("/account", authenticateToken, authenticateRole("admin"), AdminProcessor.getAllAccounts);
 adminRoutes.get("/account/:id", authenticateToken, authenticateRole("admin"), AdminProcessor.getAccount);
-adminRoutes.post("/account/:id/role", authenticateToken, authenticateRole("admin"), AdminProcessor.postAccountRole);
+adminRoutes.put("/account/:id/role", authenticateToken, authenticateRole("admin"), AdminProcessor.postAccountRole);
 
 export default adminRoutes;
